6247dda02638fde694e443f87619e987861bdcc8
Caio Vinicius <aio_vinicius11111@hotmail.com>
2021-02-14 18:01:41 -0300

    first commit

---
+++ urna/etapas.js
@@ -0,0 +1,61 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Pinguim',
+                partido: 'DDD',
+                fotos: [
+                    {url:'Pinguim.jpg', legenda: 'Vereador' }
+                ]
+            },
+           
+        {
+            numero: '77711',
+            nome: 'Duas Caras',
+            partido: 'RRR',
+            fotos: [
+                {url: 'Duas caras.jpg', legenda: 'Vereador'}
+            ]
+        },
+      ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '77',
+                nome: 'Batman',
+                partido: 'GTM',
+                vice: 'Mulher Gato',
+                fotos: [
+                    {url: 'Batman.jpg', legenda: 'Prefeito'},
+                    {url: 'robin.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '99',
+                nome: 'Coringa',
+                partido: 'THIEF',
+                vice: 'Duas Caras',
+                fotos: [
+                    {url: 'Coringa.png', legenda: 'Prefeito'},
+                    {url: 'charada.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '66',
+                nome: 'Mulher Gato',
+                partido: 'MILK',
+                vice: 'Hera Venenosa',
+                fotos: [
+                    {url: 'Mulher Gato.jpg', legenda: 'Prefeito'},
+                    {url: 'hera venenosa.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
